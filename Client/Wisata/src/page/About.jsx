18bd0630f7a68531c0af93f1677b6869b40944e6
Riki Pajri <109595793+Rikpaj@users.noreply.github.com>
2026-04-26 23:10:24 +0700

    Update About.jsx

--- Client/Wisata/src/page/About.jsx
+++ Client/Wisata/src/page/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
                 <div className="w1/" style={{backgroundImage: `url(${background2}) `}}>
                   <p className="font-Lato text-justify text-29446F px-96 pl-56 mr-20 w-full h-72 p-14 font-['Lato'] " style={{backgroundImage: `url(${background0}) `}}>
                   <h3 className=" flex card text-2x1 text-justify Baskerville sm:text-3xl text-2xl font-bold title-font mb-4 text-gray-900 font-['Baskerville']">MISI</h3>
-                    Memberikan pengalaman perjalanan yang tak terlupakan. Kami percaya bahwa setiap perjalanan adalah kesempatan untuk belajar, tumbuh, dan menghargai keindahan keberagaman dunia.Kami berkomitmen untuk memberikan layanan terbaik, mempromosikan keberlanjutan, dan mendukung komunitas lokal di destinasi wisata.</p>
+                    Memberikan pengalaman perjalanan yang tak terlupakan. Kami percaya bahwa setiap perjalanan adalah kesempatan untuk belajar, tumbuh, dan menghargai keindahan keberagaman dunia.Kami berkomitmen untuk memberikan layanan terbaik, mempromosikan keberlanjutan, dan mendukung komunitas lokal di destinasi wisata</p>
                   <OurTeam/>
                   <Footer/>
                 </div>
